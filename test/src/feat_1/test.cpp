a39108caaaec71d9377eef7ef68036fa8cc73d60
Charles-AntoineLaurin <charles-antoine.laurin@polymtl.ca>
2024-08-17 04:10:09 -0400

    Ajout de fichier de tests

---
+++ test/src/feat_1/test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Test1, TestCase1) {
+    EXPECT_TRUE(true) << "Test senser fonctionner";
+}
---
+++ test/src/feat_2/test2.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+
+TEST(Test2, TestCase2) {
+    EXPECT_TRUE(false) << "Test senser fail";
+}
